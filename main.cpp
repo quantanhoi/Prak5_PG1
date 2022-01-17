20b416bbbfcbf55c9412c2c829ac85c26456900f
quantanhoi <thieuquangtrung1999@gmail.com>
2022-01-17 02:03:53 +0100

    update

--- main.cpp
+++ main.cpp
@@ -11,15 +11,21 @@ private:
     int Maximalgewicht;
     int MinGewicht;
     double Wachstumsrate;
-    double Vermehrrate;
+    int Vermehrrate;
+
 protected:
     std::string Rasse;
-public:
 
+public:
+    bool dead{false};
+    bool breeding{false};
     void initialisieren();   //initialisiert den Park mit gegebenen Info
-    void age();     //prüfen, ob die Tieren ausgewachsen haben oder nicht
+    bool checkAge();     //prüfen, ob die Tieren ausgewachsen haben oder nicht
     void Alterungsschritt();    //wachsen
-    void Altersschwaeche();     //schwächen
+    void Altersschwaeche();     //schwächen und sterben
+    void breed();
+    void breedGewicht();
+
     int getGewicht();
     int getMaxGewicht();
     int getMinGewicht();
@@ -33,7 +39,7 @@ public:
 
 class Hebivoren:public Tieren {
 private:
-    double Versteckchance;
+    int Versteckchance;
 public:
     void setVersteckchance();
     bool hide();
@@ -41,13 +47,69 @@ public:
     double getVersteckchance();
 };
 class Karnivoren:public Tieren {
+private:
+    int FailedHuntCounter;
 public:
     void hunt();
     void extraInfo();       //extra information für Initialiesieren
+    Karnivoren() {
+        FailedHuntCounter = 0;
+    }
 
 };
-void printPark(std::vector<Hebivoren>& Hebi, std::vector<Karnivoren>& Karni);
 
+
+
+void CreatingKarni(std::vector<Karnivoren>& Karni, Karnivoren& k1, std::string& Race);
+void CreatingHebi (std::vector<Hebivoren>& Hebi, Hebivoren& h1, std::string& Race );
+void Initating(std::vector<Hebivoren>& Hebi, std::vector<Karnivoren>& Karni, Hebivoren& h1, Karnivoren& k1);
+void printPark(std::vector<Hebivoren>& Hebi, std::vector<Karnivoren>& Karni);
+void removeDead(std::vector<Hebivoren>& Hebi, std::vector<Karnivoren>& Karni);
+
+
+//Tieren Quellcode
+
+/*
+Breed... lmao es gibt zu wenig Information dazu, kann ein einziges Tier sich vermehren?
+oder braucht es immer noch 2 oder mehr Tieren der gleichen Art, um zu vermehren
+(dann brauchen wir auch ein Counter)?
+Jedoch gehoert breed() zu Klass Tieren (nach Aufgabe), wie kann ich ein Counter fuer Vector von Tieren erstellen ?? :D ??
+*/
+void Tieren::breed() {     //ein Tier vermehrt sich wie eine Zelle...
+    int random;
+    random = rand()%100;
+    if(random <= Vermehrrate) {
+        breeding = true;
+    }
+}
+bool Tieren::checkAge() {
+    if(Gewicht == Maximalgewicht) {
+        return true;
+    }
+    else return false;
+}
+
+void Tieren::Alterungsschritt() {
+    if(Gewicht < Maximalgewicht) {
+        Gewicht = Gewicht*Wachstumsrate;
+        if(Gewicht > Maximalgewicht) {
+            Gewicht = Maximalgewicht;
+        }
+    }
+}
+void Tieren::Altersschwaeche() {
+    int random{};
+    int dropDead = (Wachstumsrate*100) - 100;
+    if(checkAge()) {
+        random = rand()%100;
+        if(random <= dropDead) {
+            dead = true;
+        }
+    }
+}
+void Tieren::breedGewicht() {
+    Gewicht = MinGewicht;
+}
 int Tieren::getGewicht() {
     return Gewicht;
 }
@@ -66,7 +128,7 @@ double Tieren::getWachstumsrate() {
 double Tieren::getVermehrrate() {
     return Vermehrrate;
 }
-void Tieren::printInfo() {
+/*void Tieren::printInfo() {
     log(getGewicht());
     log(getRasse());
     log(getMaxGewicht());
@@ -74,10 +136,10 @@ void Tieren::printInfo() {
     log(getWachstumsrate());
     log(getVermehrrate());
     log("End");
-}
+}*/
 std::ostream& operator<<(std::ostream& stream, Tieren& Tier) {    //practicing operator overloading
-    stream << "Gewicht " << Tier.getGewicht() << "\nRasse " << Tier.getRasse() << "\nMax Gewicht " << Tier.getMaxGewicht() << "\nMin Gewicht " << Tier.getMinGewicht()
-           << "\nWachstumsrate " << Tier.getWachstumsrate() << "\nVermehrrate " << Tier.getVermehrrate();
+    stream << "Rasse " << Tier.getRasse() << "\nGewicht " << Tier.getGewicht() << "\nMax Gewicht " << Tier.getMaxGewicht() << "\nMin Gewicht " << Tier.getMinGewicht()
+           << "\nWachstumsrate " << Tier.getWachstumsrate() << "\nVermehrrate " << Tier.getVermehrrate() <<"%";
     return stream;
 }
 void Tieren::setInfo(std::string& Race) {
@@ -85,49 +147,56 @@ void Tieren::setInfo(std::string& Race) {
         Rasse = "Brachiosaurus";
         Maximalgewicht = 30000;
         MinGewicht = (5*Maximalgewicht) / 100;
-        Wachstumsrate = 0.2;
-        Vermehrrate = 0.15;
+        Wachstumsrate = 1.2;
+        Vermehrrate = 15;
     }
     if(Race == "Parasaurolophus") {
         Rasse = "Parasaurolophus";
         Maximalgewicht = 1500;
         MinGewicht = (5*Maximalgewicht) / 100;
-        Wachstumsrate = 0.4;
-        Vermehrrate = 0.85;
+        Wachstumsrate = 1.4;
+        Vermehrrate = 85;
     }
     if(Race == "Raptor") {
         Rasse ="Raptor";
         Maximalgewicht = 500;
         MinGewicht = (20 * Maximalgewicht) / 100;
-        Wachstumsrate = 0.3;
-        Vermehrrate = 0.12;
+        Wachstumsrate = 1.3;
+        Vermehrrate = 12;
     }
     if(Race == "Tyrannosaurus Rex") {
         Rasse ="Tyrannosaurus Rex";
         Maximalgewicht = 8000;
         MinGewicht = (20 * Maximalgewicht) / 100;
-        Wachstumsrate = 0.2;
-        Vermehrrate = 0.08;
+        Wachstumsrate = 1.2;
+        Vermehrrate = 8;
     }
 
 }
 void Tieren::initialisieren() {
     Gewicht = rand()%(Maximalgewicht - MinGewicht) + MinGewicht;
-
 }
+
+
+//Hebivoren Quellcode
 
 void Hebivoren::setVersteckchance() {
     if(getRasse() == "Brachiosaurus") {
-        Versteckchance = 0.5;
+        Versteckchance = 50;
     }
     if(getRasse() == "Parasaurolophus") {
-        Versteckchance = 0.75;
+        Versteckchance = 75;
     }
 }
 double Hebivoren::getVersteckchance() {
     return Versteckchance;
 }
 
+//Karnivoren Quellcode
+
+
+
+//Alle Funktionen Quellcode
 
 
 void CreatingHebi (std::vector<Hebivoren>& Hebi, Hebivoren& h1, std::string& Race ) {
@@ -139,6 +208,17 @@ void CreatingHebi (std::vector<Hebivoren>& Hebi, Hebivoren& h1, std::string& Rac
 void CreatingKarni(std::vector<Karnivoren>& Karni, Karnivoren& k1, std::string& Race) {
     k1.setInfo(Race);
     k1.initialisieren();
+    Karni.push_back(k1);
+}
+void breedHebi (std::vector<Hebivoren>& Hebi, Hebivoren& h1, std::string Race ) {
+    h1.setInfo(Race);
+    h1.setVersteckchance();
+    h1.breedGewicht();
+    Hebi.push_back(h1);
+}
+void breedKarni (std::vector<Karnivoren>& Karni, Karnivoren& k1, std::string Race) {
+    k1.setInfo(Race);
+    k1.breedGewicht();
     Karni.push_back(k1);
 }
 void Initating(std::vector<Hebivoren>& Hebi, std::vector<Karnivoren>& Karni, Hebivoren& h1, Karnivoren& k1) {
@@ -159,26 +239,58 @@ void Initating(std::vector<Hebivoren>& Hebi, std::vector<Karnivoren>& Karni, Heb
     for(int i = 0; i < 2; i++) {
         CreatingKarni(Karni, k1, Race);
     }
-    printPark(Hebi, Karni);
+    //printPark(Hebi, Karni);             //checking the list of all inititated animal in the park
 
+}
+void passingTime(std::vector<Hebivoren>& Hebi, std::vector<Karnivoren>& Karni, Hebivoren& h1, Karnivoren& k1) {
+    for(unsigned int i{}; i < Hebi.size(); i++) {
+        Hebi.at(i).Alterungsschritt();
+        if(Hebi.at(i).checkAge()) {
+            Hebi.at(i).Altersschwaeche();
+        }
+        if(!Hebi.at(i).dead) {
+            Hebi.at(i).breed();
+            if(Hebi.at(i).breeding) {
+                breedHebi(Hebi, h1, Hebi.at(i).getRasse());
+                std::cout << "A wild "<< Hebi.at(i).getRasse() << " has been born" << std::endl;
+            }
+        }
+    }
 }
 void printPark(std::vector<Hebivoren>& Hebi, std::vector<Karnivoren>& Karni) {
     for(unsigned int i = 0; i < Hebi.size(); i ++ ) {      //print out all information to check
             std::cout << Hebi.at(i) << std::endl;
-            std::cout << Hebi.at(i).getVersteckchance() << std::endl;
+            std::cout << "Vermehrrate: " <<Hebi.at(i).getVersteckchance() << std::endl<< std::endl;
         }
         for(unsigned int i = 0; i < Karni.size(); i ++ ) {
-            std::cout << Karni.at(i) <<std::endl;
+            std::cout << Karni.at(i) <<std::endl<< std::endl;
         }
+}
+void removeDead(std::vector<Hebivoren>& Hebi, std::vector<Karnivoren>& Karni) {
+    for(unsigned int i = 0; i < Hebi.size(); i ++ ) {
+        if(Hebi.at(i).dead) {
+            Hebi.erase(Hebi.begin()+i);
+            std::cout << "A " << Hebi.at(i).getRasse() << " is dead" << std::endl ;
+        }
+    }
+    for(unsigned int i = 0; i < Karni.size(); i ++ ) {
+        if(Karni.at(i).dead) {
+            Karni.erase(Karni.begin()+i);
+            std::cout << "A " << Karni.at(i).getRasse() << " is dead" << std::endl;
+        }
+    }
 }
 
 int main()
 {
     srand(time(NULL));
-   std::vector<Hebivoren> Hebi;
-   std::vector<Karnivoren> Karni;
-   Hebivoren h1;
-   Karnivoren k1;
-   Initating(Hebi, Karni, h1, k1);
+    std::vector<Hebivoren> Hebi;
+    std::vector<Karnivoren> Karni;
+    Hebivoren h1;
+    Karnivoren k1;
+    Initating(Hebi, Karni, h1, k1);
+
+    passingTime(Hebi,Karni, h1, k1);
+    printPark(Hebi, Karni);
 }
 
