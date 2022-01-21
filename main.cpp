ce15d38fec90ccaf7e9342d2b5dbdb39f36f3590
quantanhoi <thieuquangtrung1999@gmail.com>
2022-01-21 01:44:24 +0100

    updating on loop

--- main.cpp
+++ main.cpp
@@ -8,21 +8,30 @@ int main()
     Karnivoren k1;
     int BrachiCount{}, ParaCount{}, RaptorCount{}, TrexCount{};
     int passMonth{};
+    char fortsetzen{};
     Initating(Hebi, Karni, h1, k1);
 
-    while(true) {
+    while(true) {      //loop to see how many month has passed
+        fortsetzen = 'c';
         std::cout << "Wie viele Monate sind vergangen?: " ;
         std::cin >> passMonth;
+        if(passMonth <= 0) {
+            continue;
+        }
         for(int i{}; i < passMonth; i++) {
             passingTime(Hebi,Karni, h1, k1);
         }
         printPark(Hebi, Karni, BrachiCount, ParaCount, RaptorCount, TrexCount);
         log(passMonth << " Monate sind vergangen...");
         std::cout << "Brachio: " << BrachiCount << "\nPara: " << ParaCount << "\nRaptor: " << RaptorCount << "\nTrex: " << TrexCount << std::endl;
+        while(fortsetzen!= 'y' && fortsetzen!= 'n') {
+            std::cout << "Continue? (y/n): ";
+            std::cin >> fortsetzen;
+            if(fortsetzen == 'n') {
+                return 0;
+            }
+        }
     }
-
-
-
-
+    return 0;
 }
 
